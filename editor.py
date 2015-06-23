9e0f5f37c29a2dc3742fd09fe7e1e5e7c2cd1b70
LukeNookem <lookenookem@gmail.com>
2015-06-23 15:13:52 -0400

    Lines truncate, simplified clearing of lines

--- editor.py
+++ editor.py
@@ -2,11 +2,12 @@
 from __future__ import print_function
 
 from sys import exit
-from utils import getch, moveUp, moveDown, moveRight, moveLeft, clear
+from utils import getch, moveUp, moveDown, moveRight, moveLeft, clear, clearLine, getTerminalSize
 from os import getcwd, listdir
 from os.path import isfile, join
 
 import re
+
 def fuzzyfinder(user_input, collection):
 	suggestions = []
 	pattern = '.*?'.join(user_input)
@@ -24,6 +25,8 @@ def runCommand(command):
 		exit(0)
 	elif baseCommand == "print":
 		return [" ".join(args), ""]
+	elif baseCommand == "testResult":
+		return [None, " ".join(args)]
 	else:
 		return [None, "Command not recognized"]
 
@@ -31,14 +34,15 @@ def checkFiles():
 	return [ f for f in listdir(getcwd()) if isfile(join(getcwd(), f)) ]
 
 def mainLoop():
+	(width, height) = getTerminalSize()
 	stringIn, stringInPrev, commandResultPrev = "", "", ""
 	prompt = ">"
 	commandPrefix = "* "
 	commandResult = commandPrefix
 	paths = checkFiles()
 	results = fuzzyfinder("", paths)
-	resultsPrev = results
-	print(commandPrefix + '\n' + prompt + '\n' + str(fuzzyfinder("", paths)) + moveUp(2))
+	temp_results = (str(results)[:width - 3] + '...') if len(str(results)) > (width - 3) else str(results)
+	print(commandPrefix + '\n' + prompt + '\n' + temp_results + moveUp(2))
 	while True:
 		charIn = getch()
 		stringInPrev = stringIn
@@ -46,8 +50,8 @@ def mainLoop():
 		commandResultPrev = commandResult
 
 		if charIn == '\x03' or charIn == '\x1A': 
-			print("Please exit with the '/quit' command\n")
-		elif charIn == "": # There is a backspace character in here
+			print(clear() + "Please exit with the '/quit' command\n")
+		elif charIn == '\x7F' or charIn == '\x08': # x7f is technically delete, but lots of systems use it as backspace
 			stringIn = stringIn[:-1]
 		elif charIn == "\t":
 			stringIn = results[0]
@@ -73,14 +77,14 @@ def mainLoop():
 			stringIn += charIn
 
 		results = fuzzyfinder(stringIn, paths)
-		commandResultLine = "".join(" " * len(commandResultPrev)) + "\r" + commandResult
-		stringInLine = "".join(" " * len(stringInPrev)) + "\r" + prompt + stringIn
-		resultsLine = "".join(" " * len(str(resultsPrev))) + "\r" + str(results)
+		commandResultLine = (commandResult[:width - 3] + '...') if len(commandResult) > (width - 3) else commandResult
+		stringInLine = (stringIn[:width - 3 - len(prompt)] + '...') if len(stringIn) > (width - 3 - len(prompt)) else stringIn
+		resultsLine = (str(results)[:width - 3] + '...') if len(str(results)) > (width - 3) else str(results)
 
 		print(moveUp(1) + \
-			commandResultLine + '\n' + \
-			prompt + stringInLine + '\n' + \
-			resultsLine + \
+			clearLine() + commandResultLine + '\n' + \
+			clearLine() + prompt + stringInLine + '\n' + \
+			clearLine() + resultsLine + \
 			moveUp(1), end='\r')
 
 mainLoop()
--- utils.py
+++ utils.py
@@ -52,3 +52,29 @@ def moveLeft(num):
 
 def clear():
 	return chr(27) + '[2J'
+
+def clearLine():
+	return chr(27) + '[K'
+
+def getTerminalSize():
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+    return int(cr[1]), int(cr[0])
